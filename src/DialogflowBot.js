0a8b6bf5622e3daad462f1ef8c1586df5581fb3e
Luis Cascante <luisalonzocascante@gmail.com>
2019-07-11 12:43:35 -0600

    step-2: hour intent

--- src/DialogflowBot.js
+++ src/DialogflowBot.js
@@ -1,13 +1,23 @@
 import { WebhookClient, Text } from "dialogflow-fulfillment";
 
+const HOUR_INTENT = "HourIntent";
+
 export default class DialogflowBot {
   constructor() {
     this.actionMap = new Map();
+    this.actionMap.set(HOUR_INTENT, this.getCurrentTime.bind(this));
   }
 
   async handle(req, res) {
     // Se crea el WebhookClient que manejará las solicitudes basándose en el map
-    const agent = new WebhookClient({ request, response });
+    const agent = new WebhookClient({ request: req, response: res });
     await agent.handleRequest(this.actionMap);
+  }
+
+  getCurrentTime(agent) {
+    const today = new Date();
+    const currentTime = today.getHours() + ":" + today.getMinutes();
+    const text = new Text(currentTime);
+    agent.add(text);
   }
 }
